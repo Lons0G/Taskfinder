362444c91552406dfd1bd697098a79b66123cf64
Angel Galarza <angel_alonso1@outlook.es>
2022-08-04 14:29:13 -0500

    Agregado tabs al menu principal

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
 import PySimpleGUI as sg      
+import categoria_model 
 
 #Datos de prueba
+#_categorias = categoria_model.Read_Categoria()
+#print(_categorias[0][1])
 data = [ 
     ['Actividad']
 ]
 #Organizacion de los componentes de la ventana
-left_layout = [
+Actividad_layout = [
     [sg.Text('Actividad'), sg.Push(),sg.Input(key='-Task-', size = (30, 1))],
     [sg.Text('Descripcion'), sg.Push(), sg.Input(key='-Desc-', size = (30, 1))],
     [sg.CalendarButton('Fecha', format='%d-%m-%Y'), sg.Push(), sg.In('', key = '-Date-', size = (30, 1))],
+    #[sg.Combo(_categorias[0][1], key='-Combo-')]
     [sg.Submit()]
 ]
-
+Categoria_layout = [
+    [sg.Text('Categoria'), sg.Push(),sg.Input(key='-Categoria-', size = (30, 1))],
+]
 right_part = [ 
     [sg.Table(values=data, auto_size_columns = False, def_col_width = 30, justification = 'center')]    
 ]
 
+tabgroup = [
+    [sg.TabGroup([[ 
+                     sg.Tab('Actividad', Actividad_layout, title_color = 'White'),
+                     sg.Tab('Categoria', Categoria_layout, title_color= 'white')
+                 ]],
+                    tab_location= 'left'
+                 )]
+]
+
 layout = [
-    [sg.Column(left_layout, vertical_alignment = 'top'), 
+    [sg.Column(tabgroup, vertical_alignment = 'top'), 
         sg.VerticalSeparator(), 
         sg.Column(right_part, vertical_alignment = 'top')],
 ]      
