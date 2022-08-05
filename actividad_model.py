26752ebeba3a19466f42dbe6e66639681b99403d
Angel Galarza <angel_alonso1@outlook.es>
2022-08-04 22:17:50 -0500

    Agregado la funcion de visualizar actividad

---
+++ actividad_model.py
@@ -0,0 +1,59 @@
+import sqlite3
+
+# Insercion de Actividad :Parametros (Nombre, Descripcion, Fecha, Hora, IdCategoria)
+def Insert_Actividad(Nombre, Descripcion, Fecha, Hora, Categoria):
+    try:
+        conection = sqlite3.connect('taskfinder.db')
+        conection.execute('''
+            INSERT INTO Actividad(Nombre, Descripcion, Fecha, Hora, Categoria) VALUES (?, ?, ?, ?, ?)''', (Nombre, Descripcion, Fecha, Hora, Categoria))
+        conection.commit()
+        print('Se guardo correctamente')
+    except Exception as error:
+        print('Error es: ' + str(error))
+    finally:
+        conection.close()
+
+# Captura de Activdad 
+def Read_Actividad():
+    result = []
+    conection = sqlite3.connect('taskfinder.db')
+    data = conection.execute('''
+        SELECT * FROM Actividad 
+    ''')
+    for row in data:
+        result.append(list(row))
+    conection.commit()
+    conection.close()
+    
+    return result
+
+# Actualizacion de la Actividad :Parametros (Id, Nombre, Descripcion, Fecha, Hora, IdCategoria)
+def Update_Actividad(Id, Nombre, Descripcion, Fecha, Hora, Categoria):
+    data = (Id, Nombre, Descripcion, Fecha, Hora, Categoria)
+    try:
+        conection = sqlite3.connect('taskfinder.db')
+        query = ('''
+            UPDATE Actividad SET Nombre = ?, Descripcion = ?, Fecha = ?, Hora = ?, IdCategoria = ? WHERE Id = ?
+        ''')
+        conection.execute(query, data)
+        conection.commit()
+        print('Se actualizo correctamente')
+    except Exception as error:
+        print('Error es: ' + str(error))
+    finally: 
+        conection.close()
+
+# Eliminacion de Activdad :Parametros (Id)
+def Delete_Actividad(Id):
+    try:
+        conection = sqlite3.connect('taskfinder.db')
+        conection.execute('''
+            DELETE Activdad WHERE Id = ? 
+        ''', Id)
+        conection.commit()
+        print('Se elimino correctamente')
+    except Exception as error:
+        print('Error es: ' + str(error))
+    finally:
+        conection.close()
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import PySimpleGUI as sg      
 import categoria_model 
+import actividad_model
 
 #Datos de prueba
 def get_categorias():
@@ -11,10 +12,7 @@ def get_categorias():
 
 c_data = categoria_model.Read_Categoria()
 _categorias = get_categorias()
-a_data = [ 
-    [1, 'Actividad']
-]
-
+a_data = actividad_model.Read_Actividad()  
 #Organizacion de los componentes de la ventana
 A_left_layout = [
     [sg.Text('Actividad'), sg.Push(),sg.Input(key='-Task-', size = (30, 1))],
@@ -25,7 +23,7 @@ A_left_layout = [
     [sg.Submit()]
 ]
 A_right_layout = [ 
-    [sg.Table(values = a_data, headings = ['Id', 'Actividad'] ,auto_size_columns = False, col_widths= (8, 30), justification = 'center')]    
+    [sg.Table(values = a_data, headings = ['Id', 'Actividad'] ,auto_size_columns = False, col_widths= (5, 30), justification = 'center')]    
 ]
 Actividad_layout = [
     [sg.Column(A_left_layout, vertical_alignment = 'top'),
@@ -37,7 +35,7 @@ C_left_layout = [
     [sg.Ok('Guardar')]
 ]
 C_right_layout = [
-    [sg.Table(values = c_data, headings = ['Id', 'Categoria'],auto_size_columns = False, col_widths = (8, 30), justification = 'center', key = '-Cat-')]    
+    [sg.Table(values = c_data, headings = ['Id', 'Categoria'],auto_size_columns = False, col_widths = (5, 30), justification = 'center', key = '-Cat-')]    
 ]
 Categoria_layout = [
     [sg.Column(C_left_layout, vertical_alignment = 'top'),
